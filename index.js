2ecb047e728ce3ce4388d83093d1cb91a4d912f4
Otabek <otash4447771@gmail.com>
2020-10-26 03:02:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//let answer= confirm("хотите подписаться на сайт");
+//console.log(answer);
+/*let answer= prompt("Если вам +18", "да",);
+console.log(answer);*/
+let answer= //prompt("Если вам +18", "да",);
+console.log(typeof(null));
